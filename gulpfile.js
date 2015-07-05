2758a7fc5db36f4488131dbbc8b2759d1443c5ab
Tenderfeel <tenderfeel@gmail.com>
2015-07-05 17:06:30 +0900

    webpackの設定をwebpack.config.jsに移す

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,8 @@ var runSequence = require('run-sequence');
 var browserSync = require('browser-sync');
 
 var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
 
-var node_dir = __dirname + '/node_modules';
 
 gulp.task('html', function() {
   return gulp.src('src/*.html')
@@ -25,49 +25,8 @@ webpack
 http://webpack.github.io/docs/
 */
 gulp.task('webpack', function() {
-  webpack({
-     entry: {
-        main: ["./src/js/main.js"],
-        vendor: ['jquery']
-      },
-      //出力されるファイル
-      output: {
-        path:"./dist/js",
-        filename: "[name].js"
-      },
-      resolve: {
-        modulesDirectories: ['node_modules'],
-        //aliasを貼るとrequire('jquery');のようにパス無しでつかえる
-        alias: {
-            'jquery': node_dir + '/jquery/dist/jquery.js',
-            'underscore': node_dir + '/underscore/underscore.js',
-            'backbone': node_dir + '/backbone/backbone.js'
-        }
-      }
-    // ,externals: {
-    //   'jquery': 'jQuery'
-    // }
-    ,plugins: [
-        //<script>で読み込む順番にしないと"webpackJsonp"がないエラー出る・・・
-        new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
-        new webpack.optimize.CommonsChunkPlugin('main','main.js'),
-        // ライブラリ間で依存しているモジュールが重複している場合、二重に読み込まないようにする
-        new webpack.optimize.DedupePlugin()
-        //グローバルに出す設定。requireせず使えるようになる
-        ,new webpack.ProvidePlugin({
-            $: 'jquery',
-             _: 'underscore',
-            Backbone: 'backbone'
-        })
-        //Minimize all JavaScript output of chunks
-        //圧縮ここでやるお
-        ,new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        })
-    ]
-   }, function(err, stats) {
+  var config = Object.create(webpackConfig);
+  webpack(config , function(err, stats) {
         if(err) throw new gutil.PluginError("webpack", err);
     })
 
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+
+var node_dir = __dirname + '/node_modules';
+var webpack = require('webpack');
+
+module.exports = {
+   entry: {
+      main: ["./src/js/main.js"],
+      vendor: ['jquery']
+    },
+    //出力されるファイル
+    output: {
+      path:"./dist/js",
+      filename: "[name].js"
+    },
+    resolve: {
+      modulesDirectories: ['node_modules'],
+      //aliasを貼るとrequire('jquery');のようにパス無しでつかえる
+      alias: {
+          'jquery': node_dir + '/jquery/dist/jquery.js',
+          'underscore': node_dir + '/underscore/underscore.js',
+          'backbone': node_dir + '/backbone/backbone.js'
+      }
+    }
+  // ,externals: {
+  //   'jquery': 'jQuery'
+  // }
+  ,plugins: [
+      //<script>で読み込む順番にしないと"webpackJsonp"がないエラー出る・・・
+      new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
+      new webpack.optimize.CommonsChunkPlugin('main','main.js'),
+      // ライブラリ間で依存しているモジュールが重複している場合、二重に読み込まないようにする
+      new webpack.optimize.DedupePlugin()
+      //グローバルに出す設定。requireせず使えるようになる
+      ,new webpack.ProvidePlugin({
+          $: 'jquery',
+           _: 'underscore',
+          Backbone: 'backbone'
+      })
+      //Minimize all JavaScript output of chunks
+      //圧縮ここでやるお
+      ,new webpack.optimize.UglifyJsPlugin({
+          compress: {
+              warnings: false
+          }
+      })
+  ]
+};
